00b94f6bb74e5e74c4bf92d99a0cb35d624ad84e
geostojanov <gestojanov@gmail.com>
2022-12-30 06:48:03 +0100

    Firebase config setup

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore' 
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAonI_MkLNSjO15wXporUDjtBnS_jVyTI0",
+  authDomain: "fb9-reading-list-39244.firebaseapp.com",
+  projectId: "fb9-reading-list-39244",
+  storageBucket: "fb9-reading-list-39244.appspot.com",
+  messagingSenderId: "694150490961",
+  appId: "1:694150490961:web:3143f62e1b72f43ac70170"
+};
+
+// init firebase
+initializeApp(firebaseConfig)
+
+// init services
+const db = getFirestore()
+
+export { db }
